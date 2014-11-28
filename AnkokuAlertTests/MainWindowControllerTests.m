c283336c68b4968a96f83ed98af832a2c6c2086a
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-11-28 18:27:48 +0900

    Updated project settings.

--- AnkokuAlertTests/MainWindowControllerTests.m
+++ AnkokuAlertTests/MainWindowControllerTests.m
@@ -16,39 +16,56 @@
 
 @implementation MainWindowControllerTests
 
-- (void)setUp {
+-(void)setUp
+{
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown {
+-(void)tearDown
+{
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 /*
-- (void)testExample {
+   - (void)testExample {
     // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
-}
+   }
 
-- (void)testPerformanceExample {
+   - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
-}
-*/
+   }
+ */
 
-- (MainWindowController *)instance
+-(MainWindowController*)instance
 {
     return [[MainWindowController alloc] init];
 }
 
-- (void)testFirstMatchStringWithRegexpPattern
+-(void)testFirstMatchStringWithRegexpPattern
 {
-    NSString *matched = [[self instance] firstMatchStringWithRegexpPattern:@"(b.d)" inString:@"abcde"];
-    XCTAssert([matched isEqualToString:@"bcd"], @"");
+    [self examinePattern:@"(b.d)" inString:@"abcde" expected:@"bcd"];
+
+    [self examinePattern:@".*(lv\\d+)" inString:@"http://live.nicovideo.jp/watch/lv201600109" expected:@"lv201600109"];
+    [self examinePattern:@".*(lv\\d+)" inString:@"lv201600109" expected:@"lv201600109"];
+
+    [self examinePattern:@".*(co\\d+)" inString:@"http://com.nicovideo.jp/community/co105127" expected:@"co105127"];
+    [self examinePattern:@".*(co\\d+)" inString:@"co105127" expected:@"co105127"];
+
+    [self examinePattern:@"https?:\\/\\/ch\\..+\\/([ -~]{4,})" inString:@"http://ch.nicovideo.jp/noriradi" expected:@"noriradi"];
+    // [self examinePattern:@"([ -~]{4,})" inString:@"noriradi" expected:@"noriradi"];
+}
+
+-(void)examinePattern:(NSString*)pattern inString:(NSString*)inString expected:(NSString*)expected
+{
+    NSString* matched = [[self instance] firstMatchStringWithRegexpPattern:pattern inString:inString];
+    XCTAssert([matched isEqualToString:expected], @"");
+    NSLog(@"pattern[%@],string[%@],expected[%@],matched[%@]", pattern, inString, expected, matched);
 }
 
 @end
